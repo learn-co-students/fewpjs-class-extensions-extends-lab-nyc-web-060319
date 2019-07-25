b855464c0989d4b811e664d25390fb6e6b92c3a8
Alexander Riccio <test35965+learn@gmail.com>
2019-07-25 15:32:30 -0400

    solve

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+"use strict";
 // Your code here
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+    
+    get perimeter() {
+        return this.sides.reduce((l,r) => {return l+r}, 0);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.sides[0] === this.sides[1]) &&(this.sides[1] === this.sides[2])) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return (this.sides[0] === this.sides[1]);
+    }
+
+    get area() {
+        return this.sides[0] * this.sides[1];
+    }
+}
