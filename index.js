b3f61ed7387c016cb4b21eb9f2997c3ce2a2eeef
github username <github email address>
2019-08-04 15:24:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+class Polygon {
+    constructor(integers) {
+        this.integers = integers;
+    }
+
+    get countSides() {
+        return this.integers.length
+    }
+
+    get perimeter() {
+        let perimeterTotal = 0
+        for(let i = 0; i < this.integers.length; i++){
+            perimeterTotal += parseInt(this.integers[i])           
+        }
+        return perimeterTotal
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.integers[0] === this.integers[1]) && (this.integers[1] === this.integers[2])) {
+            return true;
+        }
+        return false;
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return (this.integers[0] === this.integers[1]);
+    }
+    
+    get area() {
+        let side1 = this.integers[0]
+        let side2 = this.integers[1]
+        let area = side1 * side2
+        return area  
+    }
+}
